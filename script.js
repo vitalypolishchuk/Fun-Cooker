b69c77b0935078173ae5383e366a3957a06c7d39
vitalypolishchuk <vetal.polishuk@gmail.com>
2022-08-14 10:27:04 +0300

    'next page btn'

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 ///////// DOCUMENT VARIABLES /////////
-// ===== nav ===== //
+const btnContainer = document.querySelector(".btn-container");
+const nextPageBtn = document.querySelector(".next-page");
+
+// ===== RECIPE PANEL ===== //
 const panelRecipies = document.querySelector(".panel-recipes");
 
 const numOfRecipes = panelRecipies.children.length;
+let displayPage = 1;
 
 function setRecipesSmallScreen(displayPage = 1) {
-  console.log("small");
+  console.log(numOfRecipes);
   if (!numOfRecipes) panelRecipies.style.height = 0;
   if (numOfRecipes <= 3) panelRecipies.style.height = 70 * numOfRecipes + "px";
   if (numOfRecipes > 3 && numOfRecipes <= 10) {
@@ -16,33 +20,40 @@ function setRecipesSmallScreen(displayPage = 1) {
     panelRecipies.style.height = 70 * 3 + "px";
     panelRecipies.style.overflowY = "scroll";
     [...panelRecipies.children].forEach((child, index) => {
-      console.log(displayPage * 9);
       if (index >= displayPage * 10 - 10 && index <= displayPage * 9) {
-        console.log(index);
         child.classList.remove("none");
       } else child.classList.add("none");
     });
   }
+  if (numOfRecipes > displayPage * 10) btnContainer.classList.remove("none");
 }
 function setRecipesBigScreen(displayPage = 1) {
-  console.log("big");
   if (numOfRecipes <= 10) {
     panelRecipies.style.height = 70 * numOfRecipes + "px";
-    panelRecipies.style.overflowY = "visble";
+    panelRecipies.style.overflowY = "hidden";
   }
   if (numOfRecipes > 10) {
-    panelRecipies.style.height = 70 * 3 + "px";
-    panelRecipies.style.overflowY = "visble";
+    panelRecipies.style.height = 70 * 11 + "px";
+    panelRecipies.style.overflowY = "hidden";
     [...panelRecipies.children].forEach((child, index) => {
       if (index > displayPage * 9 || index < displayPage * 10 - 10) child.classList.add("none");
+      else child.classList.remove("none");
     });
   }
+  if (numOfRecipes > displayPage * 10) btnContainer.classList.remove("none");
 }
-if (window.innerWidth < 930) setRecipesSmallScreen();
-if (window.innerWidth >= 930) setRecipesBigScreen();
+function setRecipePanel(displayPage) {
+  if (window.innerWidth < 930) setRecipesSmallScreen(displayPage);
+  if (window.innerWidth >= 930) setRecipesBigScreen(displayPage);
+}
+setRecipePanel();
 
 window.addEventListener("resize", function () {
   // display found recipes on screen
-  if (this.screen.width < 930) setRecipesSmallScreen();
-  if (this.screen.width >= 930) setRecipesBigScreen();
+  setRecipePanel();
+});
+
+nextPageBtn.addEventListener("click", function () {
+  displayPage++;
+  setRecipePanel(displayPage);
 });
